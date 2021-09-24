d84a7ae013e4028ea584f504bf342bde55e33f6f
shisui <gwk24yt@gmail.com>
2021-09-24 10:10:44 +0530

    🔢 Some cool stuff

--- Number/factorial.java
+++ Number/factorial.java
@@ -1,13 +1,21 @@
+
+// Importing the Scanner class
 import java.util.Scanner;
 
 public class factorial {
     public static void main(String[] args) {
+        // Creating the scanner object to get user's input
         Scanner sc = new Scanner(System.in);
 
+        // Asking for the integer.
         System.out.println("Input a number to get its factorial");
-        int n = sc.nextInt(), factorial = 1;
-        int number = n;
+        // Storing the integer
+        int n = sc.nextInt();
 
+        // Declaring the required variables
+        int factorial = 1, number = n;
+
+        // Getting the factorial
         while (n > 0) {
             factorial *= n;
             n--;
